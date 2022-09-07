6d286e78e742e8ae3afeb208adeef6e9ee674e3b
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2022-09-06 23:47:36 -0500

    feat: criar models

---
+++ ModeloPortalAgora.Api/Entities/ContaBancaria.cs
@@ -0,0 +1,21 @@
+﻿namespace ModeloPortalAgora.Api.Entities
+{
+    public class ContaBancaria
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        public int Digito { get; set; }
+        public int Banco { get; set; }
+    }
+
+    public class Controlador
+    {
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public int Participacao { get; set; }
+        public string PontroladorPai { get; set; }
+    }
+}
